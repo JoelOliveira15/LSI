a0223a43c57943410f5e5c53dbfefd24e31ae486
JoelSistemas2015 <joelsistemasti@gmail.com>
2016-11-07 18:11:08 -0300

    Adicionando alguns scripts

---
+++ Site/menu_site.php
@@ -0,0 +1,14 @@
+﻿			<!-- MENU -->
+							<nav id="nav">
+								<ul>
+									<li><a class="icon " href="index.php"><span>Inicio</span></a></li>
+						<!-- Porque a clinc med -->
+									<li><a class = "icon " href = "#"><span>Porque ClinicMed?</span></a></li>
+						<!-- O que é clinc med -->
+									<li><a class = "icon " href = "#"><span>O que é ClinicMed?</span></a></li>
+						<!-- Contatos -->
+									<li><a class = "icon " href = "#"><span>Contatos</span></a></li>
+						<!-- login -->
+									<li><a class = "icon" href = "login.php"><span>Login</span></a></li>
+								</ul>
+								</nav>
---
+++ Tabelas/Usuario.sql
@@ -0,0 +1,12 @@
+﻿create table Usuario(
+
+idUsuario int primary key,
+nome varchar not null,
+login int not null,
+senha varchar not null,
+data_nascimento date not null,
+endereco varchar not null,
+telefone int not null,
+email varchar not null
+
+);
